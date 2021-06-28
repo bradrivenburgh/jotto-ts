56e9cae41b6273349f9592a1382f11ba86079555
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-06-28 16:36:12 -0400

    Add Congrats component and test utils

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const welcomeMsg = screen.getByText(/hi there/i);
+  expect(welcomeMsg).toBeInTheDocument();
 });
---
+++ src/components/Congrats.tsx
@@ -0,0 +1,21 @@
+interface CongratsProps {
+  success: boolean;
+}
+/**
+ * @function
+ * @param {object} props - React props.
+ * @returns {JSX.Element} - Rendered component (or null if 'success' prop is false)
+ */
+const Congrats: React.FC<CongratsProps> = ({ success }) => {
+  return success ? (
+    <div data-test='component-congrats'>
+      <span data-test='congrats-message'>
+        Congratulations! You guessed the word!
+      </span>
+    </div>
+  ) : (
+    <div data-test='component-congrats'></div>
+  );
+};
+
+export default Congrats;
