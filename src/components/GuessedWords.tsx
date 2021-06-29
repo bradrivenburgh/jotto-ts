8327e85463a69ea9d849f3ee9c1be41c4b11b814
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-06-29 15:53:25 -0400

    Tests and code for non-zero guessed words

--- src/components/GuessedWords.tsx
+++ src/components/GuessedWords.tsx
@@ -3,11 +3,29 @@ interface GuessedWordsProps {
 }
 
 const GuessedWords: React.FC<GuessedWordsProps> = ({ guessedWords }) => {
+  const guessedWordsRows = guessedWords.map((word, i) => (
+    <tr data-test='guessed-word' key={i}>
+      <td>word.guessedWord</td>
+      <td>word.letterMatchCount</td>
+    </tr>
+  ));
+
   let contents =
     guessedWords.length === 0 ? (
       <span data-test='guess-instructions'>Try to guess the secret word!</span>
     ) : (
-      ''
+      <div data-test='guessed-words'>
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>MatchingLetters</th>
+            </tr>
+          </thead>
+          <tbody>{ guessedWordsRows }</tbody>
+        </table>
+      </div>
     );
 
   return <div data-test='component-guessed-words'>{contents}</div>;
