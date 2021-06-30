c631df25b3cb44478cf06eed772b5bc137657be5
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-06-30 14:59:13 -0400

    mock of useState works but need to add cleanup

--- src/components/Input.test.tsx
+++ src/components/Input.test.tsx
@@ -4,6 +4,7 @@ import Input from './Input';
 
 // Mock React.useState
 const mockSetCurrentGuess = jest.fn(); // mock setter function
+
 jest.mock('react', () => ({
   //require the actual 'react' package
   ...jest.requireActual('react'),
@@ -25,14 +26,26 @@ test('renders without crashing', () => {
 });
 
 describe('state controlled input field', () => {
-  test('state updates with value of input box upon change', () => {
-    const wrapper = setup();
-    const inputBox = findByTestAttr(wrapper, 'input-box');
+  let wrapper: ShallowWrapper;
+  beforeEach(() => {
+    mockSetCurrentGuess.mockClear();
+    wrapper = setup();
+  });
 
+  test('state updates with value of input box upon change', () => {
+    // Find input
+    const inputBox = findByTestAttr(wrapper, 'input-box');
     // Simulate change event to "input-box" with value of "train"
     const mockEvent = { target: { value: 'train' } };
     inputBox.simulate('change', mockEvent);
 
     expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+  });
+
+  test('setCurrentGuess is called with an empty string onSubmit', () => {
+    const submitButton = findByTestAttr(wrapper, 'submit-button');
+    submitButton.simulate('click');
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('');
   });
 });
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import React, { useState } from 'react';
 
 interface InputProps {
   secretWord: string;
@@ -7,9 +7,18 @@ interface InputProps {
 const Input: React.FC<InputProps> = ({ secretWord }) => {
   const [currentGuess, setCurrentGuess] = useState('');
 
+  const onClick = () => {
+    setCurrentGuess(''); // Reset input
+
+    // TODO: Update the guessedWords global state
+
+    // TODO: Check against "secretWord" and optionally update "success"
+    // global state
+  };
+
   return (
     <div data-test='component-input'>
-      <form className='form-inline'>
+      <form data-test='form' className='form-inline'>
         <input
           data-test='input-box'
           className='mb-2 mx-sm-3'
@@ -18,7 +27,10 @@ const Input: React.FC<InputProps> = ({ secretWord }) => {
           value={currentGuess}
           onChange={(e) => setCurrentGuess(e.target.value)}
         />
-        <button data-test='submit-button' className='btn btn-primary mb-2'>
+        <button
+          data-test='submit-button'
+          className='btn btn-primary mb-2'
+          onClick={onClick}>
           Submit
         </button>
       </form>
