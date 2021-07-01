b488913ef5927058bd48a572aed51e22d57851d2
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-07-01 10:20:56 -0400

    Test App
    Change App test to use Enzyme instead of React Testing Library

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { shallow, ShallowWrapper } from 'enzyme';
+import { findByTestAttr } from '../__test__/testUtils';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const appTitle = screen.getByText(/Jotto/i);
-  expect(appTitle).toBeInTheDocument();
+const setup = (): ShallowWrapper => {
+  return shallow(<App />);
+};
+
+test('renders without error', () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
+  expect(appComponent.length).toBe(1);
 });
