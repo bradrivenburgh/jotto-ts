8f072d24fc71f9ec8769ab0c6f6f8b3072fae646
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-07-06 17:02:14 -0400

    fixed warnings

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,6 @@
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 
-export const getSecretWord = (): Promise<AxiosResponse<any>> => {
+export const getSecretWord = (): any => {
   // TODO: write actual function in Redux section
   return axios.get('http://localhost:3030').then((response) => response.data);
 };
