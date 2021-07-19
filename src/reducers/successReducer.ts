3f8d278ff54d2da522cb1bae4bb09978b4f97209
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-07-19 16:28:53 -0400

    Tests and Code
    Add successReducer tests and code

--- src/reducers/successReducer.ts
+++ src/reducers/successReducer.ts
@@ -1,5 +1,15 @@
-const reducer = (state: string[], action: { type: string }) => {
-  return null;
+import { actionTypes } from '../actions';
+
+const reducer = (
+  state: undefined | boolean = false,
+  action: { type: string }
+) => {
+  switch (action.type) {
+    case actionTypes.CORRECT_GUESS:
+      return true;
+    default:
+      return state;
+  }
 };
 
 export default reducer;
